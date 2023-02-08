f9fc86ceec750ae0420de4de0c25006b0ce9c12d
JohanesKristiadi <johanes.kristiadi@student.umn.ac.id>
2023-02-08 18:03:01 +0700

    Add Header.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,24 +2,24 @@ import React from "react";
 
 // membuat komponen dengan menggunakan class
 
-// class App extends React.Component{
-//     render(){
-//         return(
-//             <div>
-//                 <h1>hello World</h1>
-//             </div>
-//         )
-//     }
-// }
+class App extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>hello World</h1>
+            </div>
+        )
+    }
+}
 
 // membuat komponen dengan menggunakan function
 
-const App = () => {
-    return (
-        <div>
-            <h1>hello world</h1>
-        </div>
-    )
-};
+// const App = () => {
+//     return (
+//         <div>
+//             <h1>hello world</h1>
+//         </div>
+//     )
+// };
 
 export default App;
