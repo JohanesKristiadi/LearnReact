380e29364e42119c36f096fb842cfcb431bcdddd
JohanesKristiadi <johanes.kristiadi@student.umn.ac.id>
2023-02-08 17:49:11 +0700

    create component with function

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,25 @@
-import React from "react"
-class App extends React.Component{
-    render(){
-        return(
-            <div>
-                <h1>hello World</h1>
-            </div>
-        )
-    }
-}
+import React from "react";
+
+// membuat komponen dengan menggunakan class
+
+// class App extends React.Component{
+//     render(){
+//         return(
+//             <div>
+//                 <h1>hello World</h1>
+//             </div>
+//         )
+//     }
+// }
+
+// membuat komponen dengan menggunakan function
+
+const App = () => {
+    return (
+        <div>
+            <h1>hello world</h1>
+        </div>
+    )
+};
 
 export default App;
